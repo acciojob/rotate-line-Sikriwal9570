8facf25312ceae134ab08f96db96c90f36b8cbaa
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-10-04 10:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let rotationAngle = 0;
+
+function rotateLine() {
+    rotationAngle += 2;
+    line.style.transform = `translateX(-50%) translateY(-50%) rotate(${rotationAngle}deg)`;
+    setTimeout(rotateLine, 20);
+}
+
+rotateLine();
+
